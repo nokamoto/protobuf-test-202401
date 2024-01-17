e8b491026f780fac18d5a838e2e10c7ea26ebe7b
Naofumi Okamoto <nokamoto.engr@gmail.com>
2024-01-17 11:11:40 +0000

    repeated superset

--- main.go
+++ main.go
@@ -11,11 +11,23 @@ import (
 )
 
 func main() {
+	fmt.Println("diff v1.A -> v2.B")
+
 	a := v1.A{
 		A: "a",
 		B: &v1.SuperSet{
 			First: 100,
 			Third: 300,
+		},
+		C: []*v1.SuperSet{
+			{
+				First: 100,
+				Third: 300,
+			},
+			{
+				First: 100 + 1,
+				Third: 300 + 1,
+			},
 		},
 	}
 
@@ -23,6 +35,17 @@ func main() {
 
 	var b v2.B
 	proto.Unmarshal(bytes, &b)
+	fmt.Println(cmp.Diff(&a, &b, protocmp.Transform()))
 
-	fmt.Println("diff", cmp.Diff(&a, &b, protocmp.Transform()))
+	fmt.Println("diff v2.B -> v1.A")
+
+	b.B.Second = 200
+	b.C[0].Second = 200
+	b.C[1].Second = 200 + 1
+
+	bytes, _ = proto.Marshal(&b)
+
+	var a2 v1.A
+	proto.Unmarshal(bytes, &a2)
+	fmt.Println(cmp.Diff(&b, &a2, protocmp.Transform()))
 }
